9dbad12d7e0fbc4262fa8889e207a6a7a9ff5013
Alex Ronchi Dutra <alex.leleco@gmail.com>
2018-12-27 18:23:31 -0200

    teste

--- src/config/server.js
+++ src/config/server.js
@@ -10,9 +10,7 @@ server.use(bodyParser.json())
 server.use(allowCors)
 server.use(queryParser())
 
-
 server.listen(port, function () {
     console.log(`BACKEND is running on port ${port}.`)
 })
-
 module.exports = server
